d809ddaeff26dd95e280f808e586da42422136bd
Joel Pinto <joelpostman97@gmail.com>
2022-05-03 22:25:41 +0100

    mqtt: Implement mqtt auth values decryption and mosquitto remote authetication
    This commit implements the authentication values decryption with the aid of a custom
    encryptation shared library. The plain values are used to perform remote broker authentication.
    This commit also resolve the linkage with the custom encryptation library.
    
    Signed-off-by: Joel Pinto <joelpostman97@gmail.com>

--- src/mqtt/fd_mosquitto.cxx
+++ src/mqtt/fd_mosquitto.cxx
@@ -2,6 +2,7 @@
 #include <cstdio>
 
 #include "fd_mosquitto.h"
+#include "c_crypt.h"
 
 fd_mosquitto::fd_mosquitto(std::vector<std::string> &topics, const std::string &host)
     : m_topics(topics)
@@ -16,8 +17,19 @@ fd_mosquitto::~fd_mosquitto()
     mosqpp::lib_cleanup();
 }
 
-int fd_mosquitto::connect()
+int fd_mosquitto::connect(std::vector<int> &user, std::vector<int> &pw)
 {
+    if (!user.empty() && !pw.empty()) {
+        char u_plain[user.size()+1], pw_plain[pw.size()+1];
+        int u_crpt[user.size()], pw_crpt[pw.size()];
+
+        std::copy(user.begin(), user.end(), u_crpt);
+        std::copy(pw.begin(), pw.end(), pw_crpt);
+
+        c_decrypt(u_crpt, user.size(), u_plain);
+        c_decrypt(pw_crpt, pw.size(), pw_plain);
+        baseclass::username_pw_set(u_plain, pw_plain);
+    }
 
     return baseclass::connect(m_host.c_str());
 }
